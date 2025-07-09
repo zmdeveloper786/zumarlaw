917a0d1567253a1b1cd1e6ca8d310ff79a4a30c4
zmdeveloper786 <zm019651@gmail.com>
2025-07-09 01:41:44 -0700

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -37,59 +37,59 @@
 // }));
 
 
-import dotenv from 'dotenv';
-dotenv.config(); // must come before accessing process.env
+// import dotenv from 'dotenv';
+// dotenv.config(); // must come before accessing process.env
 
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+// import passport from 'passport';
+// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+// import jwt from 'jsonwebtoken';
+// import User from '../models/User.js';
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    const googleId = profile.id;
-    const email = profile.emails?.[0]?.value;
-    const firstName = profile.name?.givenName || '';
-    const lastName = profile.name?.familyName || '';
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: process.env.GOOGLE_CALLBACK_URL,
+// },
+// async (accessToken, refreshToken, profile, done) => {
+//   try {
+//     const googleId = profile.id;
+//     const email = profile.emails?.[0]?.value;
+//     const firstName = profile.name?.givenName || '';
+//     const lastName = profile.name?.familyName || '';
 
-    if (!email) {
-      return done(new Error("Google account did not provide an email."), null);
-    }
+//     if (!email) {
+//       return done(new Error("Google account did not provide an email."), null);
+//     }
 
-    // Check for existing user by email
-    let user = await User.findOne({ email });
+//     // Check for existing user by email
+//     let user = await User.findOne({ email });
 
-    if (user) {
-      // If user exists but no googleId, update it
-      if (!user.googleId) {
-        user.googleId = googleId;
-        await user.save();
-      }
-    } else {
-      // Create new user
-      user = await User.create({
-        googleId,
-        email,
-        firstName,
-        lastName
-      });
-    }
+//     if (user) {
+//       // If user exists but no googleId, update it
+//       if (!user.googleId) {
+//         user.googleId = googleId;
+//         await user.save();
+//       }
+//     } else {
+//       // Create new user
+//       user = await User.create({
+//         googleId,
+//         email,
+//         firstName,
+//         lastName
+//       });
+//     }
 
-    const token = jwt.sign(
-      { id: user._id, email: user.email },
-      process.env.JWT_SECRET,
-      { expiresIn: '1d' }
-    );
+//     const token = jwt.sign(
+//       { id: user._id, email: user.email },
+//       process.env.JWT_SECRET,
+//       { expiresIn: '1d' }
+//     );
 
-    done(null, { user, token });
+//     done(null, { user, token });
 
-  } catch (err) {
-    console.error("Error in Google Strategy:", err);
-    done(err, null);
-  }
-}));
+//   } catch (err) {
+//     console.error("Error in Google Strategy:", err);
+//     done(err, null);
+//   }
+// }));
